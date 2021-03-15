3cb30101ced96ed13e3af014369ccd193aa63ec4
alehhunko <alehhunko@gmail.com>
2021-03-15 14:58:17 +0300

    fix: reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) {
+        n = -n;}
+        let i=String(n);
+        let t=i.split("").reverse().join("");
+        return Number(t);
 }
